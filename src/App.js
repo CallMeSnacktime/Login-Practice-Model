e971e6877525b729851743b10af6522c8cae61b5
Atiba Lashley <teebz25@gmail.com>
2020-05-11 19:54:18 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import './App.css'
+
+const App= () => {
+    const [ username, setUsername] = useState('');
+    const [ password, setPassword] = useState('');   
+    const [ showPassword, setShowPassword] = useState(true);   
+    return (
+        <div className='container'>
+            <input 
+                type='text'
+                placeholder='Enter username'
+                value={username}
+                // use an event to set user name
+                onChange={(e)=>{setUsername(e.target.value ) }}
+                    
+            />
+            <input 
+                // Toggles password viability
+                type= {showPassword? 'text':'password'}
+                placeholder='Enter Password'
+                value={password}
+                // use an event to set user name
+                onChange={(e)=>{ setPassword(e.target.value ) }}         
+            />
+            <h2>{username}</h2>
+            <h2>{showPassword? password:'*'.repeat(password.length)}</h2>
+            <button onClick={(e)=>{
+                setShowPassword(!showPassword)
+            }}>Show/Hide Password</button>
+        </div>
+    )
+}
+
+export default App;
